8a129aa580d60f9ef52fbcc7e01e9f21438347de
Inditex <inditex@P20059.local>
2021-07-05 12:56:06 +0200

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,33 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'pipes';
+
+  nombre: string = "Capitán América";
+  nombre2: string = "miGUel áNgEL gArcíA";
+  arreglo = [1,2,3,4,5,6,7,8,9,10];
+  PI: number = Math.PI;
+  porcentaje: number = 0.234;
+  salario: number = 1234.5;
+  fecha: Date = new Date();
+
+  idioma: string = "es";
+  videoUrl: string = 'https://www.youtube.com/embed/_ZXjT43__XQ'
+
+  valorPromesa = new Promise<string>((resolve) => {
+
+    setTimeout(()=> {
+      resolve('Llego la data')
+    }, 4500)
+  })
+
+  heroe = {
+    nombre: 'Logan',
+    clave: 'Wolverin',
+    edad: 500,
+    direccion: {
+      calle: 'Primera',
+      casa: 20
+    }
+  }
+
 }
